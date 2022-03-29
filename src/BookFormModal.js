cf7ef202714253d1e5d2c026b32899ad7c1ffa1d
Cole Gibbs <colegibbs0@gmail.com>
2022-03-29 16:07:43 -0700

    Form button correction

--- src/BookFormModal.js
+++ src/BookFormModal.js
@@ -4,6 +4,7 @@ import { Modal, Button, Form } from 'react-bootstrap';
 class BookFormModal extends React.Component{
 
   handleNewBook = (e) => {
+    e.preventDefault();
     console.log(e.target.title.value);
     let newBook = {
       title: e.target.title.value,
@@ -21,7 +22,7 @@ class BookFormModal extends React.Component{
           <Modal.Title>Add A New Book</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={this.handleNewBook}>
             <Form.Group controlId="title">
               <Form.Label>
                 Book Title
@@ -40,17 +41,15 @@ class BookFormModal extends React.Component{
               <Form.Check type="checkbox" label="Read"/>
             </Form.Group>
 
-            <Form.Group controlId="emial">
+            <Form.Group controlId="email">
               <Form.Label>
                 Your Email
               </Form.Label>
               <Form.Control type="email" placeholder="Your Email"/>
             </Form.Group>
+            <Button type="submit" variant="dark">Create Book</Button>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="dark" onClick={this.handleNewBook}>Create Book</Button>
-        </Modal.Footer>
       </Modal>
 
     )

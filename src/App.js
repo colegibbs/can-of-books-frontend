11cc54ea86c383836d2f1204a70821c8a3f29132
Cole Gibbs <colegibbs0@gmail.com>
2022-03-28 17:57:30 -0700

    book data and Carousel

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import BestBooks from './BestBooks';
 
 class App extends React.Component {
 
@@ -34,8 +35,10 @@ class App extends React.Component {
       <>
         <Router>
           <Header user={this.state.user} onLogout={this.logoutHandler} />
+          <BestBooks/>
           <Switch>
             <Route exact path="/">
+              {/* <BestBooks/> */}
               {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
             </Route>
             {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel';
+
+let SERVER = process.env.REACT_APP_SERVER;
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -8,21 +12,64 @@ class BestBooks extends React.Component {
     }
   }
 
+  getBooks = async () => {
+    try {
+      let url = `${SERVER}/books?title=Musashi`
+      let books = await axios.get(url);
+      this.setState({
+        books: books.data,
+      })
+    } catch(error) {
+      console.log(error);
+    }
+    // let apiUrl = `${SERVER}/books`
+    // if(title) {
+    //   apiUrl += `?title = ${title}`
+    // }
+    // console.log(apiUrl);
+    // try {
+    //   const response = await axios.get(apiUrl);
+    //   this.setState({
+    //     books: response.data,
+    //   });
+    // } catch(error) {
+    //   console.log(error);
+    // }
+  }
+
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
+  componentDidMount() {
+    this.getBooks();
+  }
 
   render() {
-
+    let books = this.state.books.map((book,idx) =>{ 
+        return(
+        <Carousel.Item key={idx}>
+          <Carousel.Caption>
+            <h3>stuff</h3>
+            <p>{book.title}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        )
+    })
+    console.log(this.state);
     /* TODO: render user's books in a Carousel */
-
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        <Carousel>
+          {books}
+        </Carousel>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
+        {/* {this.state.books.length > 0 && 
+          <>
+            {books}
+          </> */}
+        {/* //  : (
+        //   <h3>No Books Found :(</h3>
+        // )
+        } */}
       </>
     )
   }
